785c6493e771af0059f1a230799c5540ab23b819
crispynoodlesoup <thepanman321@gmail.com>
2023-09-10 14:13:41 -0500

    add a little error handling logic

--- app.js
+++ app.js
@@ -2,28 +2,37 @@ const input = document.querySelector("input");
 const button = document.querySelector("button");
 const temp = document.querySelector(".temp");
 const weatherIcon = document.querySelector(".weather-icon");
-const locationOutput = document.querySelector(".location"); 
+const locationOutput = document.querySelector(".location");
 
 let weatherInfo = {};
 
 async function getTemp() {
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=e85ba524ea4a4f8a84a21032230909&q=${input.value}&aqi=no`, { mode: "cors"});
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=e85ba524ea4a4f8a84a21032230909&q=${input.value}&aqi=no`,
+      { mode: "cors" }
+    );
     const json = await response.json();
-    
+    console.log(json);
+  
+    if (json.error) throw json.error.message; // don't continue if bad request
+  
     weatherInfo.tempF = json.current.temp_f;
     weatherInfo.location = json.location.name;
     weatherInfo.icon = json.current.condition.icon;
     weatherInfo.condition = json.current.condition.text;
-
+  
     applyWeatherInfo();
+  } catch(error) {
+    console.log(error);
+  }
 }
 
 function applyWeatherInfo() {
-    temp.innerText = `${weatherInfo.tempF}°F`;
-    weatherIcon.src = weatherInfo.icon;
-    weatherIcon.alt = weatherInfo.condition;
-    locationOutput.innerText = `${weatherInfo.location}`;
-
+  temp.innerText = `${weatherInfo.tempF}°F`;
+  weatherIcon.src = weatherInfo.icon;
+  weatherIcon.alt = weatherInfo.condition;
+  locationOutput.innerText = `${weatherInfo.location}`;
 }
 
 button.addEventListener("click", getTemp);
