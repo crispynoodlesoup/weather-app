06d5cc01af1a56b04880dd1f04f064fa4954608a
crispynoodlesoup <thepanman321@gmail.com>
2023-09-10 13:07:53 -0500

    restructure js to store json data in an object

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const button = document.querySelector("button");
 const temp = document.querySelector(".temp");
 const locationOutput = document.querySelector(".location"); 
 
+let weatherInfo = {};
+
 async function getTemp() {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=e85ba524ea4a4f8a84a21032230909&q=${input.value}&aqi=no`, { mode: "cors"});
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=e85ba524ea4a4f8a84a21032230909&q=${input.value}&aqi=no`, { mode: "cors"});
     const json = await response.json();
     
     console.log(json);
-    temp.innerText = `${json.current.temp_f}°F`;
-    locationOutput.innerText = `${json.location.name}`;
+    weatherInfo.tempF = json.current.temp_f;
+    weatherInfo.location = json.location.name;
+    weatherInfo.icon = json.current.condition.icon;
+
+    applyWeatherInfo();
+}
+
+function applyWeatherInfo() {
+    temp.innerText = `${weatherInfo.tempF}°F`;
+    locationOutput.innerText = `${weatherInfo.location}`;
+
 }
 
 button.addEventListener("click", getTemp);
