b0dac593fd43b6bda3c028dd67a8b6eda098a98a
crispynoodlesoup <thepanman321@gmail.com>
2023-09-09 20:55:05 -0500

    add location to data

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+const input = document.querySelector("input");
 const button = document.querySelector("button");
 const temp = document.querySelector(".temp");
-const input = document.querySelector("input");
+const locationOutput = document.querySelector(".location"); 
 
 async function getTemp() {
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=e85ba524ea4a4f8a84a21032230909&q=${input.value}&aqi=no`, { mode: "cors"});
     const json = await response.json();
     
+    console.log(json);
     temp.innerText = `${json.current.temp_f}°F`;
+    locationOutput.innerText = `${json.location.name}`;
 }
 
 button.addEventListener("click", getTemp);
